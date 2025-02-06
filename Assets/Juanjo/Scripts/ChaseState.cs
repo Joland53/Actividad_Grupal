b2e573fdcaf23e0253ac2c693749a8b0510ca837
Joland53 <berbelnajasjj@gmail.com>
2025-02-06 12:28:17 +0100

    Update Maquina de Estados
    Se ha implementado en la máquina de estados que si el Player sale del ángulo de visión del Enemy o un obstáculo le impide ver al jugador, este vuelva al estado de patrullar

--- Assets/Juanjo/Scripts/ChaseState.cs
+++ Assets/Juanjo/Scripts/ChaseState.cs
@@ -19,6 +19,28 @@ public class ChaseState : State<EnemyController>
 
     public override void OnUpdateState()
     {
+        if (controller.Target == null)
+        {
+            Debug.Log("Jugador perdido, volviendo a patrullar.");
+            controller.ChangeState(controller.PatrolState);
+            return;
+        }
+
+        float distancia = Vector3.Distance(controller.transform.position, controller.Target.position);
+
+        // Verificar si el jugador está aún dentro del ángulo de visión y sin obstáculos.
+        Vector3 direccionATarget = (controller.Target.position - controller.transform.position).normalized;
+        bool jugadoreEnVision = Vector3.Angle(controller.transform.forward, direccionATarget) <= controller.AnguloVision / 2;
+        bool obstaculoEnVision = Physics.Raycast(controller.transform.position, direccionATarget, distancia, controller.QueEsObstaculo);
+
+        if (!jugadoreEnVision || obstaculoEnVision)
+        {
+            Debug.Log("Jugador fuera de visión u obtáculo boloqueando, volviendo a patrullar.");
+            controller.ChangeState(controller.PatrolState);
+            return;
+        }
+
+        // Si el jugador sige en rango y visión, entonces persigue.
         controller.Agent.SetDestination(controller.Target.position);
 
         // No tengo calculos pendientes y mi distancia hacia el target es menor que la distancia de parada.
--- Assets/Juanjo/Scripts/EnemyController.cs
+++ Assets/Juanjo/Scripts/EnemyController.cs
@@ -41,11 +41,6 @@ public class EnemyController : Controller
 
         ChangeState(patrolState);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
